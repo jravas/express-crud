ffad8b0ace66c55a9b60da6453458214422ffd14
jravas <josip.ravas.broj@gmail.com>
2018-06-15 12:47:52 +0200

    access control

--- routes/articles.js
+++ routes/articles.js
@@ -3,9 +3,10 @@ const router = express.Router()
 
 // bring in models
 let Article = require('../models/article')
+let User = require('../models/user')
 
 // add article route
-router.get('/add', (req, res) => {
+router.get('/add', ensureAutenticated, (req, res) => {
   res.render('add_article', {
     title: 'Add article'
   })
@@ -13,7 +14,7 @@ router.get('/add', (req, res) => {
 // add submit POST route
 router.post('/add', (req,res) => {
   req.checkBody('title', 'Title is required').notEmpty()
-  req.checkBody('author', 'Author is required').notEmpty()
+  // req.checkBody('author', 'Author is required').notEmpty()
   req.checkBody('body', 'Body is required').notEmpty()
   // get errors if any
   let errors = req.validationErrors()
@@ -25,7 +26,7 @@ router.post('/add', (req,res) => {
   } else {
     let article = new Article()
     article.title = req.body.title
-    article.author = req.body.author
+    article.author = req.user._id
     article.body = req.body.body
     article.save((err) => {
       if(err) {
@@ -38,15 +39,20 @@ router.post('/add', (req,res) => {
   }
 })
 // get single article editing
-router.get('/edit/:id', (req, res) => {
+router.get('/edit/:id',ensureAutenticated, (req, res) => {
   Article.findById(req.params.id, (err, article) => {
-    res.render('edit_article', {
-      article: article
-    })
+    if(article.author != req.user._id) {
+      req.flash('danger', 'Noth authorized')
+      res.redirect('/')
+    } else {
+      res.render('edit_article', {
+        article: article
+      })
+    }
   })
 })
 // update submit POST route
-router.post('/edit/:id', (req,res) => {
+router.post('/edit/:id', ensureAutenticated, (req,res) => {
   let article = {}
   article.title = req.body.title
   article.author = req.body.author
@@ -63,23 +69,45 @@ router.post('/edit/:id', (req,res) => {
 })
 // delete article
 router.delete('/:id', (req, res) => {
+  if(!req.user._id) {
+    res.status(500).send()
+  }
   let query = {_id: req.params.id}
-  Article.remove(query, (err) => {
-    if(err) {
-      console.log(err)
+  Article.findById(req.params.id, (err, article) => {
+    if(article.author != req.user._id) {
+      res.status(500).send()
     } else {
-      req.flash('success', 'Article deleted')
-      res.send('Success')
+      Article.remove(query, (err) => {
+        if(err) {
+          console.log(err)
+        } else {
+          req.flash('success', 'Article deleted')
+          res.send('Success')
+        }
+      })
     }
   })
 })
 // get single article
 router.get('/:id', (req, res) => {
   Article.findById(req.params.id, (err, article) => {
-    res.render('article', {
-      article: article
+    User.findById(article.author, (err, user) => {
+      res.render('article', {
+        article: article,
+        author: user.name
+      })
     })
   })
 })
+// access control
+function ensureAutenticated(req, res, next) {
+  if(req.isAuthenticated()){
+    return next()
+  } else {
+    req.flash('danger', 'Please login')
+    res.redirect('/users/login')
+  }
+  
+}
 
 module.exports = router
